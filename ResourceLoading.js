cf3134cbcef8996aba167db3f97dd0252c6c1799
yoiang <yo.ian.g@gmail.com>
2009-08-18 01:17:13 +0900

    allows rittai files to contain endlines adds TakeNote puzzle

--- ResourceLoading.js
+++ ResourceLoading.js
@@ -57,7 +57,7 @@ function loadPuzzle( URL )
 {
     if ( URL == '' || URL == 'Select Puzzle' )
     {
-      return;
+        return;
     }
 
     if ( gXMLHttp == null )
@@ -66,17 +66,25 @@ function loadPuzzle( URL )
         // Other browsers (including IE 7.x-8.x) ignore this
         //   when XMLHttpRequest is predefined
         if (typeof(XMLHttpRequest) == "undefined") {
-          XMLHttpRequest = function() {
-            try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
-              catch(e) {}
-            try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
-              catch(e) {}
-            try { return new ActiveXObject("Msxml2.XMLHTTP"); }
-              catch(e) {}
-            try { return new ActiveXObject("Microsoft.XMLHTTP"); }
-              catch(e) {}
-            throw new Error("This browser does not support XMLHttpRequest.");
-          };
+            XMLHttpRequest = function() {
+                try {
+                    return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+                }
+                catch(e) {}
+                try {
+                    return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+                }
+                catch(e) {}
+                try {
+                    return new ActiveXObject("Msxml2.XMLHTTP");
+                }
+                catch(e) {}
+                try {
+                    return new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                catch(e) {}
+                throw new Error("This browser does not support XMLHttpRequest.");
+            };
         }
 
         gXMLHttp = new XMLHttpRequest();
@@ -91,62 +99,66 @@ function loadPuzzleFinished()
 {
     if (gXMLHttp.readyState==4)
     {
-      var Values = gXMLHttp.responseText.toString().split(" ");
-      var FailAttempts = Values[0];
-      var X = Values[1];
-      var Y = Values[2];
-      var Z = Values[3];
-      var travValues = 4;
+        var Result = gXMLHttp.responseText.toString();
+        Result = Result.replace(/\n/g, " ");
+        Result = Result.replace(/  /g, " ");
+        Values = Result.split(" ");
+        var FailAttempts = Values[0];
+        var X = Values[1];
+        var Y = Values[2];
+        var Z = Values[3];
+        var travValues = 4;
 
-      var BlocksZ = [];
-      for( var travZ = 0; travZ < Z; travZ ++ )
-      {
-          var BlocksY = [];
-          for( var travY = 0; travY < Y; travY ++ )
-          {
-              var BlocksX = [];
-              for( var travX = 0; travX < X; travX ++ )
-              {
-                  BlocksX[ travX ] = Values[ travValues ];
-                  travValues ++;
-              }
-              BlocksY[ travY ] = BlocksX;
-          }
-          BlocksZ[ travZ ] = BlocksY;
-      }
-      gGame.createPuzzle( BlocksZ, FailAttempts)
+        var BlocksZ = [];
+        for( var travZ = 0; travZ < Z; travZ ++ )
+        {
+            var BlocksY = [];
+            for( var travY = 0; travY < Y; travY ++ )
+            {
+                var BlocksX = [];
+                for( var travX = 0; travX < X; travX ++ )
+                {
+                    BlocksX[ travX ] = Values[ travValues ];
+                    travValues ++;
+                }
+                BlocksY[ travY ] = BlocksX;
+            }
+            BlocksZ[ travZ ] = BlocksY;
+        }
+        gGame.createPuzzle( BlocksZ, FailAttempts)
     }
 }
 
 function selectPuzzle()
 {
-  loadPuzzle( document.getElementById("puzzleSelect").value );
+    loadPuzzle( document.getElementById("puzzleSelect").value );
 }
 
 function PuzzleManualSelect()
 {
-   loadPuzzle( document.getElementById('url').value )
+    loadPuzzle( document.getElementById('url').value )
 }
 
 function fillPuzzleList()
 {
-  var PuzzleNames = [
-                        'One.rittai',
-                        'test.rittai',
-                        'test2.rittai',
-                        'HollowCube.rittai'
-                  ];
+    var PuzzleNames = [
+    'One.rittai',
+    'test.rittai',
+    'test2.rittai',
+    'HollowCube.rittai',
+    'puzzles/TakeNote.rittai'
+    ];
 
-//TODO: can I request a file list???
-  var Options = '<select id="puzzleSelect" name="puzzleSelect" onChange="selectPuzzle()">';
-  Options += generatePuzzleOption( 'Select Puzzle' );
-  for( var travPuzzleNames = 0; travPuzzleNames < PuzzleNames.length; travPuzzleNames ++ )
-  {
-      Options += generatePuzzleOption( PuzzleNames[ travPuzzleNames ] );
-  }
-  Options += '</select>';
+    //TODO: can I request a file list???
+    var Options = '<select id="puzzleSelect" name="puzzleSelect" onChange="selectPuzzle()">';
+    Options += generatePuzzleOption( 'Select Puzzle' );
+    for( var travPuzzleNames = 0; travPuzzleNames < PuzzleNames.length; travPuzzleNames ++ )
+    {
+        Options += generatePuzzleOption( PuzzleNames[ travPuzzleNames ] );
+    }
+    Options += '</select>';
 
-  document.getElementById('puzzleSelectDiv').innerHTML = Options;
+    document.getElementById('puzzleSelectDiv').innerHTML = Options;
 }
 
 function generatePuzzleOption( FileName )
